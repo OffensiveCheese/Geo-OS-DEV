6a60267246b9348d56f3c26ae5a191d8a59af77e
NunoLava1998 <nunolava1998@gmail.com>
2017-02-02 22:05:09 +0000

    Add files via upload

---
+++ lib/gdt.c
@@ -0,0 +1,41 @@
+#include "sys.h"
+
+struct gdt_table {
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t base_medium;
+	uint8_t access;
+	uint8_t flags;
+	uint8_t base_high;
+} __attribute__ ((packed));
+
+struct gdtr {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__ ((packed));
+
+struct gdt_table gdt[5];
+struct gdtr gdtp;
+
+extern void lgdt(void);
+
+void gdt_set_entry(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
+	gdt[num].limit_low = (limit & 0xFFFF);
+	gdt[num].base_low = ((base >> 0) & 0xFFFF);
+	gdt[num].base_medium = ((base >> 16) & 0xFF);
+	gdt[num].access = (access & 0xFF);
+	gdt[num].flags = (((limit >> 16) & 0x0F) | (flags & 0xF0));
+	gdt[num].base_high = ((base >> 24) & 0xFF);
+}
+
+
+void gdt_load(void) {
+	gdtp.limit = (sizeof(struct gdt_table) * 5) - 1;
+	gdtp.base = (uint32_t)&gdt;
+	gdt_set_entry(0, 0, 0, 0, 0); // Null descriptor.
+	gdt_set_entry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code Segment.
+	gdt_set_entry(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data Segment.
+	gdt_set_entry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // Usermode Code Segment.
+	gdt_set_entry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // Usermode Data Segment.
+	lgdt();
+}
---
+++ lib/tty.c
@@ -0,0 +1,106 @@
+#include "sys.h"
+
+
+// We'll want to define some variables.
+
+int x = 0;
+int y = 0;
+uint16_t *videoptr = (uint16_t *)0xB8000;
+
+// Then, define our functions.
+
+size_t strlen(const char* str) {
+	size_t len = 0;
+	while (str[len] != '\0') {
+		len++;
+	}
+	return len;
+}
+
+
+void update_videoptr(void) {
+	videoptr = (uint16_t *)0xB8000 + (y * 80 + x);
+}
+
+void newline(void) {
+	x = 0;
+	y++;
+	update_videoptr();
+}
+
+void backspace(void) {
+	if (x == 0 && y == 0) {
+		x = 80;
+		y = 25;
+		update_videoptr();
+		return;
+	}
+	if (x > 0) {
+			x--;
+			update_videoptr();
+			return;
+		}
+	if (x == 0) {
+		y--;
+		x = 79;
+		update_videoptr();
+		return;
+	}
+	return;
+}
+
+void restrictions_tty(void) {
+	if (x == 80) {
+		if (y == 25) {
+			x = 0;
+			y = 0;
+			update_videoptr();
+		}
+	}
+}
+
+void writec(char c, uint8_t bc, uint8_t fc) {
+	restrictions_tty(); // Apply restrictions.
+	if (c == '\n') {
+		newline();
+		return;
+	}
+	if (c == '\b') {
+		backspace();
+		*videoptr = ' ' | (((0 << 4) | (0 & 0x0F)) << 8);
+		return;
+	}
+	uint16_t attribute = (bc << 4) | (fc & 0x0F);
+	*videoptr = c | (attribute << 8);
+	if (x == 80) {
+		newline();
+		update_videoptr();
+	} else {
+		x++;
+		update_videoptr();
+	}
+}
+
+void writes(const char* c, uint8_t bc, uint8_t fc) {
+	for (size_t i = 0; i < strlen(c); i++) {
+		writec(c[i], bc, fc);
+	}
+}
+
+void terminal_cls(void) {
+	x = 0;
+	y = 0;
+	update_videoptr();
+	for (int i = 0; i < 25; i++) {
+		for (int a = 0; a < 80; a++) {
+			writec(' ', 0, 0);
+		}
+	}
+}
+
+void terminal_init(void) {
+	terminal_cls();
+	x = 0;
+	y = 0;
+	update_videoptr();
+}
