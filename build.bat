1d4370e127201c8316d6ed31992923e25a5ac9e1
NunoLava1998 <nunolava1998@gmail.com>
2017-02-03 12:26:33 +0000

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 
 i686-elf-as lib/gdt.S -o gdt.o
-i686-elf-gcc -c kmain.c -o kmain.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror
-i686-elf-gcc -c lib/gdt.c -o gdtf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror
-i686-elf-gcc -c lib/tty.c -o ttyf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror 
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kmain.o ttyf.o gdtf.o gdt.o -lgcc
+i686-elf-gcc -c kmain.c -o kmain.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
+i686-elf-gcc -c lib/gdt.c -o gdtf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
+i686-elf-gcc -c lib/tty.c -o ttyf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kmain.o ttyf.o gdtf.o gdt.o -lgcc -g
 
 echo.
 echo Run?
