12928bc17dbcd17d3cab45012114ebf837a68d17
NunoLava1998 <nunolava1998@gmail.com>
2017-02-05 16:02:37 +0000

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,13 +1,14 @@
 @echo off
 
 i686-elf-as lib/gdt.S -o gdt.o
+i686-elf-as lib/idt.S -o idt.o
 i686-elf-gcc -c kmain.c -o kmain.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
 i686-elf-gcc -c lib/gdt.c -o gdtf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
 i686-elf-gcc -c lib/tty.c -o ttyf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
 i686-elf-gcc -c lib/idt.c -o idtf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
 i686-elf-gcc -c lib/mem.c -o memf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
 i686-elf-gcc -c lib/pic.c -o picf.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wpedantic -Wshadow -Werror -g
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kmain.o ttyf.o gdtf.o gdt.o idtf.o memf.o picf.o -lgcc -g
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kmain.o ttyf.o gdtf.o gdt.o idtf.o memf.o picf.o idt.o -lgcc -g
 
 echo.
 echo Run?
